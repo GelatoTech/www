142b4e8c6a375bde1c86909fcf4553959a1e6915
Ike <ikescode@gmail.com>
2020-09-25 13:49:02 -0500

    current state

--- pages/_app.js
+++ pages/_app.js
@@ -20,19 +20,36 @@ import '../public/javascripts/jquery.min';
 import '../public/javascripts/all';
 import '../public/javascripts/aos';
 import '../public/javascripts/font-awesome-all';
-dynamic(import ('../public/javascripts/fbevents'), { ssr:false });
+// dynamic(import ('../public/javascripts/fbevents'), { ssr:false });
 dynamic(import ('../public/javascripts/googletagmanager'), { ssr:false });
-dynamic(import ('../public/javascripts/xfbml.customerchat'), { ssr:false });
+// dynamic(import ('../public/javascripts/xfbml.customerchat'), { ssr:false });
 dynamic(import ('../public/javascripts/owl.carousel.min'), { ssr:false });
+
+// import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 
 import Nav from '../components/Nav';
+
+// import { useRef } from 'react';
 
 export default function App({ Component, pageProps }) {
   return (
     <div>
       <Nav />
+      {/* <div ref={useRef("customer-chat")}></div>
       <Component {...pageProps} />
+      <MessengerCustomerChat
+        pageId="1848657532048801"
+        appId="gelato"
+        htmlRef="customer-chat"
+      /> */}
     </div>
   )
 }
+
+
+
+
+// TODO:
+//    - uncomment fb customer chat
+//    - add google tag manager (Google Analytics)
