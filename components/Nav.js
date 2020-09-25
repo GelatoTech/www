5bb71925e1340f1ad4e2849234f333f48f6a2c30
Ike <ikescode@gmail.com>
2020-09-25 11:07:27 -0500

    import global styles, add icons

--- components/Nav.js
+++ components/Nav.js
@@ -1,30 +1,30 @@
-export default () => {
+export default function Nav() {
   return(
     <div>
       <header className="is-fixed-top">
-        <nav aria-label="main navigation" class="navbar" id='nav' role="navigation">
+        <nav aria-label="main navigation" className="navbar" id='nav' role="navigation">
           <div className="navbar-brand">
             <span>
-              <a class="navbar-item" href="/">
+              <a className="navbar-item" href="/">
                 <img alt="iphone screen repair" height="30" src="images/nav-logo-gtech.png" width="150" />
               </a>
             </span> 
-            <a aria-expanded="false" aria-label="menu" class="navbar-burger burger" data-target="navbarBasicExample" role="button">
+            <a aria-expanded="false" aria-label="menu" className="navbar-burger burger" data-target="navbarBasicExample" role="button">
               <span aria-hidden="true"></span> 
               <span aria-hidden="true"></span> 
               <span aria-hidden="true"></span>
             </a>
           </div>
-          <div class="navbar-menu has-dropdown" id="navbarBasicExample">
-            <div class="navbar-end" id="nav-items">
-              <div class="navbar-item">
-                <a class="navbar-item" href="#works">How it Works</a> <a class="navbar-item" href="#repair">Repair my iPhone <i class="fas fa-mobile" style={{marginLeft: "1vh"}}></i></a> <a class="navbar-item" href="/about" target="_blank">About Us</a>
-                <div class="navbar-item has-dropdown is-hoverable">
-                  <a class="navbar-link">Blog</a>
-                  <div class="navbar-dropdown">
-                    <a class="navbar-item" href='/batterytips' id="blog" target='_blank'>3 iOS Battery Saving Tips</a> <a class="navbar-item" href='/screenrepairblog' id="blog2" target='_blank'>How much will an iPhone screen repair cost you in San Francisco?</a>
+          <div className="navbar-menu has-dropdown" id="navbarBasicExample">
+            <div className="navbar-end" id="nav-items">
+              <div className="navbar-item">
+                <a className="navbar-item" href="#works">How it Works</a> <a className="navbar-item" href="#repair">Repair my iPhone <i className="fas fa-mobile" style={{marginLeft: "1vh"}}></i></a> <a className="navbar-item" href="/about" target="_blank">About Us</a>
+                <div className="navbar-item has-dropdown is-hoverable">
+                  <a className="navbar-link">Blog</a>
+                  <div className="navbar-dropdown">
+                    <a className="navbar-item" href='/batterytips' id="blog" target='_blank'>3 iOS Battery Saving Tips</a> <a className="navbar-item" href='/screenrepairblog' id="blog2" target='_blank'>How much will an iPhone screen repair cost you in San Francisco?</a>
                   </div>
-                </div><a class="navbar-item" href="/faq" target="_blank">FAQ</a> <a class="navbar-item" href="/legal" target="_blank">Lifetime Warranty</a> <a class="navbar-item" href="#business">Hours <i class="fas fa-clock" style={{marginLeft: "5px"}}></i></a> <a class="button is-info" href="tel:4155177897">(415) 517-7897 <i class="fas fa-phone"></i></a>
+                </div><a className="navbar-item" href="/faq" target="_blank">FAQ</a> <a className="navbar-item" href="/legal" target="_blank">Lifetime Warranty</a> <a className="navbar-item" href="#business">Hours <i className="fas fa-clock" style={{marginLeft: "5px"}}></i></a> <a className="button is-info" href="tel:4155177897">(415) 517-7897 <i className="fas fa-phone"></i></a>
               </div>
             </div>
           </div>
