d15b63f631aff4fdc520b50b7fb1363d439eb822
Ike <ikescode@gmail.com>
2021-12-13 12:51:21 -0600

    add GA tag

--- pages/_app.js
+++ pages/_app.js
@@ -33,6 +33,7 @@ export default function App({ Component, pageProps }) {
   useEffect(() => {
     // Google Analytics
     ReactGA.initialize('UA-124172320-1');
+    ReactGA.initialize('UA-121085071-1');
     ReactGA.pageview(window.location.pathname + window.location.search);
 
     // LinkedIn Insight
