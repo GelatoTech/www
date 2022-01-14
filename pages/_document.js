321b39aebb5917d3da27bd9ba23cad08c2193a91
Ike <ikescode@gmail.com>
2022-01-14 17:00:53 -0600

    use html for tawk

--- pages/_document.js
+++ pages/_document.js
@@ -28,6 +28,20 @@ export default class MyDocument extends Document {
               gtag('config', 'UA-124172320-1');`
             }}
           />
+          {/* Start of Tawk.to Script */}
+          <script dangerouslySetInnerHTML={{
+              __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/61e0fde5f7cf527e84d2116d/1fpbdviiq';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+              })();`
+            }} />
+          {/* End of Tawk.to Script */}
         </Head>
         <body>
           <Main />
