1546c5ee2cc74c5af2d52b1bb05d63c7c91191d4
SEO Professional <144199341+seoprogb@users.noreply.github.com>
2024-06-27 10:30:29 +0000

    description updated

--- features/Sitemap/Container.js
+++ features/Sitemap/Container.js
@@ -9,6 +9,7 @@ export default function Sitemap() {
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
                 <title>Sitemap | Gelatotech</title>
+                <meta name="description" content="Explore Gelatotech's comprehensive sitemap, which provides easy navigation so you can quickly and efficiently find all the pages and resources you need." />
             </Head>
             <section style={{ marginTop: '10em', height: '90vh' }}>
                 <div className="container">
