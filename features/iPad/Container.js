70cc7e16f8e97dcc4421b8b42389e008a24d9536
SEO Professional <144199341+seoprogb@users.noreply.github.com>
2024-06-27 05:43:52 +0000

    Updated Hreflang

--- features/iPad/Container.js
+++ features/iPad/Container.js
@@ -36,7 +36,7 @@ export default function iPad() {
         <meta name="google-site-verification" content="otWUpfaXKjZHurqJg8KCSsabxvEJ9380Jprvwzazg8g" />
         <link rel="stylesheet" href="/stylesheets/ipad.css" />
         <link rel="stylesheet" href="/stylesheets/device-card.css" />
-        <link rel="alternate" href="https://gelatotech.com/ipad" hreflang="en" />
+        <link rel="alternate" href="https://gelatotech.com/ipad" hreflang="en-us" />
       </Head>
       <section className="hero" id="hero">
         <div className="hero-body">
