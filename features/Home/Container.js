9b3124dc97a31e509e9b27779710ea7184eec8de
SEO Professional <144199341+seoprogb@users.noreply.github.com>
2024-07-11 09:21:14 +0000

    Hreflang updated

--- features/Home/Container.js
+++ features/Home/Container.js
@@ -27,6 +27,7 @@ export default function Home() {
       <Head>
         <title>Save Time & Money with the Best Mobile IPhone Repair Service</title>
         <link rel="alternate" href="https://gelatotech.com/" hreflang="en-us" />
+        <link rel="alternate" href="https://gelatotech.com/" hreflang="x-default"/>
         <link rel="canonical" href="https://gelatotech.com"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
